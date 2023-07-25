9002f664f3b5822621b18d77f40360a645cc9bb1
yourOnlyCode <59841414+yourOnlyCode@users.noreply.github.com>
2023-07-24 20:21:10 -0400

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+	import Painting from './components/Painting.svelte';
+  
+	// Replace these with your painting data
+	const paintings = [
+	  {
+		title: 'Ethereal',
+		subtitle: 'You Are The Sky',
+		imageUrl: 'path/to/painting1.jpg',
+		price: '$800',
+		description: 'Oil on Wood Panel'
+	  },
+	  {
+		title: 'Ethereal 2',
+		subtitle: 'You Are The Sky',
+		imageUrl: 'path/to/painting1.jpg',
+		price: '$800',
+		description: 'Oil on Wood Panel'
+	  }
+	  // Add more paintings as needed
+	];
+</script>
+  
+<main class="container mx-auto py-8">
+	<h1 class="text-3xl font-semibold mb-4">My Paintings</h1>
+	<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+	  {#each paintings as painting}
+		<Painting {...painting} />
+	  {/each}
+	</div>
+</main>
+  
+<style>
+	/* Add any custom styling here or modify existing Tailwind CSS classes */ 
+	main {
+		margin: auto;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+</style>
+  
---
+++ src/components/Painting.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let title;
+    export let subtitle;
+    export let imageUrl;
+    export let price;
+    export let description;
+</script>
+
+<div class="border rounded-lg p-4">
+    <h2 class="text-xl font-semibold mb-2">{title}</h2>
+    <h4 class="font-regular" style="margin-top: -25px;">{subtitle}</h4>
+    <img src={imageUrl} alt={title} class="w-full"/>
+    <div>{price}</div>
+    <div>{description}</div>
+</div>
